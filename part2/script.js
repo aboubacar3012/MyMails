47ced9df2bb0df53f227c1b4d2b08b2a3e833125
Aboubacar DIALLO <a.diallo3012@gmail.com>
2022-06-08 12:06:25 +0200

    Ajout d'un message, lorsque il n'ya aucun email

--- part2/script.js
+++ part2/script.js
@@ -1,6 +1,18 @@
 //! Nombre de message
-const compteur = document.getElementsByClassName("card").length;
-const nbMessage = document.getElementById("totalMessage").textContent = compteur;
+function compteur(){
+    const compteur = document.getElementsByClassName("card").length;
+    if(compteur <= 0){
+        document.querySelector(".main h2").remove();
+        console.log("vous n'avez aucun message");
+        const h2 = document.createElement("h2");
+        const text = document.createTextNode("Oupps!, Vous n'avez aucun message");
+        document.querySelector(".main").appendChild(h2.appendChild(text));
+    }else{
+        document.getElementById("totalMessage").textContent = compteur;
+    }
+    
+}
+compteur();
 //* console.log(nbMessage.textContent);
 
 //! Suppression de message
@@ -9,6 +21,7 @@ document.querySelectorAll(".trash img").forEach(element => {
     element.addEventListener("click",function(){
         // console.log(this.parentElement.parentElement);
         this.parentElement.parentElement.remove()
+        compteur();
     });
 })
 
